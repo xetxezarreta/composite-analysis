904f76c685081ef39125d5fdbeb7f4a91e0dbdea
xetxezarreta <xabi1500@gmail.com>
2019-10-18 11:58:21 +0200

    composite feature straction

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import pandas as pd
+from glob import glob
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-data = pd.read_csv("data/0.csv")
+total = ['Total_PorosityQuantity', 'Total_PorosityQuality', 'Total_UnfilledZones', 'Total_FillingQuality', 'TOTAL_QUALITY']
 
-print(data)
+path = r'D:/master/LIP/composite-analysis/data'
+all_files = glob(path + '/*.csv')
+li = []
+
+for id, filename in enumerate(all_files[:10]):
+    df = pd.read_csv(filename, index_col=None, header=0)
+    df = df.drop(axis=1, columns=total)
+    df['id'] = id
+    li.append(df)    
+
+data = pd.concat(li, axis=0, ignore_index=True)
+
+import tsfresh
+
+# extracted_features = extract_features(data, column_id='id', column_sort='Time')
